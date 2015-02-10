ebeb62866fc374f47ad0fdf9b557927270db095c
Ryan Scott <atthealma@gmail.com>
2015-02-11 08:02:49 +1100

    Added a lazy benchmark for the behaviour tree construction and evaluating

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@
 #![feature(std_misc)]
 #![feature(rand)]
 
+extern crate test;
+
 // Example based on the example provided by http://machinejs.maryrosecook.com
 
 use std::cell::RefCell;
@@ -28,12 +30,10 @@ mod example;
 use example::world::World;
 use example::tree::Tree;
 
-#[allow(dead_code)]
-fn main() {
-    let world = Rc::new(RefCell::new(World::new()));
-    let mut tree = Tree::new(world.clone());
+use test::Bencher;
 
-    let mut world_behaviour: Selector<World> = Selector::new("World Root");
+fn build_behaviour_trees() -> (Selector<'static, World>, Selector<'static, Tree>) {
+    let mut world_behaviour: Selector<'static, World> = Selector::new("World Root");
     world_behaviour.add(Box::new(Action::new("Day/Night Cycle", |world: &mut World| {
             if world.can_shine() {
                 world.toggle_sun();
@@ -54,7 +54,7 @@ fn main() {
         }
     )));
 
-    let mut tree_behaviour: Selector<Tree> = Selector::new("Tree Root");
+    let mut tree_behaviour: Selector<'static, Tree> = Selector::new("Tree Root");
 
     let mut photosynthesise: Sequence<Tree> = Sequence::new("Photosynthesise");
     photosynthesise.add(Box::new(Action::new("Make Energy", |tree: &mut Tree| {
@@ -105,6 +105,16 @@ fn main() {
         }
     )));
 
+    (world_behaviour, tree_behaviour)
+}
+
+#[allow(dead_code)]
+fn main() {
+    let world = Rc::new(RefCell::new(World::new()));
+    let mut tree = Tree::new(world.clone());
+
+    let (mut world_behaviour, mut tree_behaviour) = build_behaviour_trees();
+
     let mut timer = Timer::new().unwrap();
     loop {
         {
@@ -125,4 +135,26 @@ fn main() {
 
         timer.sleep(Duration::milliseconds(100));
     }
+}
+
+#[bench]
+fn bench_evaluate_trees_and_world(b: &mut test::Bencher) {
+    let world = Rc::new(RefCell::new(World::new()));
+    let mut tree = Tree::new(world.clone());
+
+    let (mut world_behaviour, mut tree_behaviour) = build_behaviour_trees();
+
+    b.iter(|| {
+        tree_behaviour.evaluate(&mut tree);
+
+        let mut local_world = world.borrow_mut();
+        world_behaviour.evaluate(&mut local_world)
+    })
+}
+
+#[bench]
+fn bench_build_behaviour_trees(b: &mut test::Bencher) {
+    b.iter(|| {
+        build_behaviour_trees()
+    })
 }
